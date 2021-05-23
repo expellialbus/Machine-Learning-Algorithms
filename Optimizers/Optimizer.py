72f8fd6cc1e3131a49d1db7f9199af569f17b6af
recep-yildirim <0.recepyildirim@gmail.com>
2021-05-24 01:25:57 +0300

    Partial derivative functions were added.

--- Optimizers/Optimizer.py
+++ Optimizers/Optimizer.py
@@ -1,6 +1,10 @@
+from scipy.misc import derivative
+import numpy as np
+
 from abc import ABC, abstractmethod, abstractproperty
 
 class Optimizer(ABC):
+    a = 0
     @abstractproperty
     def learning_rate(self):
         pass
@@ -12,6 +16,21 @@ class Optimizer(ABC):
     @abstractmethod
     def call(self, loss, parameter, data, labels):
         pass
+
+
+    def _partial_derivative(self, loss, parameters, data, labels):
+        gradients = list()
+        for index in range(parameters.shape[0]):
+            gradient = self.__helper_derivative(loss, index, parameters, data, labels)
+            gradients.append(gradient)
+
+        return np.array(gradients)[:, np.newaxis]  # convert gradients from (4,) shape to (4, 1) shape
+        
+    def __helper_derivative(self, loss, index, parameters, data, labels):
+        def wraps(x):
+            parameters[index] = x
+            return loss(parameters, data, labels)
+        return derivative(wraps, parameters[index])
 
     def _learning_schedule(self, t):
         return 1 / t
