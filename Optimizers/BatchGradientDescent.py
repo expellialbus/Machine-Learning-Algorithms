1b1c6f0a4652fd6ed97cc742c02ecfd73950fac9
recep-yildirim <0.recepyildirim@gmail.com>
2021-05-28 01:45:34 +0300

    Learning rate's Getter Setter methods has been moved to the base class.

--- Optimizers/BatchGradientDescent.py
+++ Optimizers/BatchGradientDescent.py
@@ -2,16 +2,8 @@ from Optimizer import Optimizer
 
 class BatchGradientDescent(Optimizer):
     def __init__(self, learning_rate, decrease_learning_rate=True):
-        self.__learning_rate = learning_rate 
+        super().__init__(learning_rate)
         self.__decrease_learning_rate = decrease_learning_rate
-
-    @property
-    def learning_rate(self):
-        return self.__learning_rate
-
-    @learning_rate.setter
-    def learning_rate(self, value):
-        self.__learning_rate = value
 
     @property
     def decrease_learning_rate(self):
@@ -19,16 +11,17 @@ class BatchGradientDescent(Optimizer):
 
     @decrease_learning_rate.setter
     def decrease_learning_rate(self, value):
-        self.__descrease_learning_rate
+        self.__descrease_learning_rate = value
 
     def call(self, loss, parameters, data, labels):
         gradients = self._partial_derivative(loss, parameters, data, labels)
-        new_parameters = self.__learning_rate * gradients
+        new_parameters = self._learning_rate * gradients
         
         if self.__decrease_learning_rate:
-            self.__learning_rate = self._learning_schedule((1 / (self.__learning_rate + 1)) * data.shape[0])
+            self._learning_rate = self._learning_schedule((1 / (self._learning_rate + 1)) * data.shape[0])
 
         return new_parameters
 
     def __call__(self, loss, parameters, data, labels):
         return self.call(loss, parameters, data, labels)
+
--- Optimizers/StochasticGradientDescent.py
+++ Optimizers/StochasticGradientDescent.py
@@ -4,16 +4,8 @@ from Optimizer import Optimizer
 
 class StochasticGradientDescent(Optimizer):
     def __init__(self, learning_rate, decrease_learning_rate=True):
-        self.__learning_rate = learning_rate
+        super().__init__(learning_rate)
         self.__decrease_learning_rate = decrease_learning_rate 
-    
-    @property
-    def learning_rate(self):
-        return self.__learning_rate
-
-    @learning_rate.setter
-    def learning_rate(self, value):
-        self.__learning_rate = value
 
     @property
     def decrease_learning_rate(self):
@@ -29,11 +21,11 @@ class StochasticGradientDescent(Optimizer):
             x_i = data[random_index: random_index + 1]        # it is equivalent to the data[random_idnex].reshape(1, -1)
             y_i = labels[random_index: random_index + 1]      # it is equivalent to the labels[random_index].reshape(1, -1)
 
-            gradients = self._partial_derivative(loss, parameters, data, labels)
-            new_parameters = self.__learning_rate * gradients
+            gradients = self._partial_derivative(loss, parameters, x_i, y_i)
+            new_parameters = self._learning_rate * gradients
 
             if self.__decrease_learning_rate:
-                self.__learning_rate = self._learning_schedule((1 / (self.__learning_rate + 1)) * data.shape[0])
+                self._learning_rate = self._learning_schedule((1 / (self._learning_rate + 1)) * data.shape[0])
 
         return new_parameters
 
