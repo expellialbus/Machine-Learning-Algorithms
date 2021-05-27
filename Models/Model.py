78cdaffa9a90278483ef799a736c1638e238c957
recep-yildirim <0.recepyildirim@gmail.com>
2021-05-28 01:49:26 +0300

    Model.py has been created.

---
+++ Models/Model.py
@@ -0,0 +1,47 @@
+import numpy as np
+
+from abc import ABC, abstractmethod
+
+class Model(ABC):
+    def __init__(self, training_steps, optimizer, loss):
+        self._training_steps = training_steps
+        self._optimizer = optimizer
+        self._loss = loss
+
+    @property 
+    def training_steps(self):
+        return self._training_steps
+    
+    @training_steps.setter
+    def training_steps(self, value):
+        self._training_steps = value
+
+    @property 
+    def optimizer(self):
+        return self._optimizer
+    
+    @optimizer.setter
+    def optimizer(self, value):
+        self._optimizer = value
+    
+    @property 
+    def loss(self):
+        return self._loss
+
+    @loss.setter
+    def loss(self, value):
+        self._value = value
+        
+    @abstractmethod
+    def call(self, data, label):
+        pass
+
+    def _initialize_model(self, data):
+        new_data = np.c_[np.ones((data.shape[0], 1)), data]
+        init_parameters = np.random.rand(new_data.shape[1], 1)
+        
+        return new_data, init_parameters
+
+    @abstractmethod
+    def calculate_loss(self, parameters, data, labels):
+        pass
