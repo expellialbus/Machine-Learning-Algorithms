2b9b24178b5a0e2eba2348e40c2527a04b68181e
recep-yildirim <0.recepyildirim@gmail.com>
2021-05-28 01:49:01 +0300

    LinearRegression has been created.

---
+++ Models/LinearRegression.py
@@ -0,0 +1,19 @@
+from Model import Model 
+
+class LinearRegression(Model):
+    def __init__(self, training_steps, optimizer, loss):
+        super().__init__(training_steps, optimizer, loss)
+    
+    def call(self, data, labels):
+        data, parameters = self._initialize_model(data)
+
+        for step in range(self._training_steps):
+            parameters -= self._optimizer(self.calculate_loss, parameters, data, labels)
+
+        return parameters
+
+    def calculate_loss(self, parameters, data, labels):
+        return self._loss(labels, data.dot(parameters))
+
+    def __call__(self, data, labels):
+        return self.call(data, labels)
