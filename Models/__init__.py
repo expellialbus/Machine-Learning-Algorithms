a8446efb36fd6ce1bf6ae750d901491865b6dbbe
recep-yildirim <0.recepyildirim@gmail.com>
2021-05-28 01:50:01 +0300

    __init__.py file has been added.

---
+++ Models/__init__.py
@@ -0,0 +1,13 @@
+import os
+
+def get_parent_dir(n=0):
+    current_path = os.path.dirname(os.path.abspath(__file__))
+
+    for _ in range(n):
+        current_path = os.path.dirname(current_path)
+
+    return current_path
+
+os.sys.path.append(get_parent_dir())
+
+from LinearRegression import LinearRegression
