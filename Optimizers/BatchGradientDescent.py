201a9fc9d7a1b01d5c4c04e3d846d5521d37fd30
recep-yildirim <0.recepyildirim@gmail.com>
2021-05-29 00:16:35 +0300

    Learning schedule method has been added.

--- Optimizers/BatchGradientDescent.py
+++ Optimizers/BatchGradientDescent.py
@@ -2,8 +2,16 @@ from Optimizer import Optimizer
 
 class BatchGradientDescent(Optimizer):
     def __init__(self, learning_rate, decrease_learning_rate=True):
-        super().__init__(learning_rate)
+        self.__learning_rate = learning_rate
         self.__decrease_learning_rate = decrease_learning_rate
+
+    @property
+    def learning_rate(self):
+        return self.__learning_rate
+
+    @learning_rate.setter
+    def learning_rate(self, value):
+        self.__learning_rate = value
 
     @property
     def decrease_learning_rate(self):
@@ -13,12 +21,15 @@ class BatchGradientDescent(Optimizer):
     def decrease_learning_rate(self, value):
         self.__descrease_learning_rate = value
 
+    def __learning_schedule(self, t):
+        return 1 / t
+    
     def call(self, loss, parameters, data, labels):
         gradients = self._partial_derivative(loss, parameters, data, labels)
-        new_parameters = self._learning_rate * gradients
+        new_parameters = self.__learning_rate * gradients
         
         if self.__decrease_learning_rate:
-            self._learning_rate = self._learning_schedule((1 / (self._learning_rate + 1)) * data.shape[0])
+            self.__learning_rate = self.__learning_schedule((1 / (self.__learning_rate + 1)) * data.shape[0])
 
         return new_parameters
 
--- Optimizers/StochasticGradientDescent.py
+++ Optimizers/StochasticGradientDescent.py
@@ -4,8 +4,16 @@ from Optimizer import Optimizer
 
 class StochasticGradientDescent(Optimizer):
     def __init__(self, learning_rate, decrease_learning_rate=True):
-        super().__init__(learning_rate)
+        self.__learning_rate = learning_rate
         self.__decrease_learning_rate = decrease_learning_rate 
+
+    @property
+    def learning_rate(self):
+        return self.__learning_rate
+
+    @learning_rate.setter
+    def learning_rate(self, value):
+        self.__learning_rate = value
 
     @property
     def decrease_learning_rate(self):
@@ -15,6 +23,9 @@ class StochasticGradientDescent(Optimizer):
     def decrease_learning_rate(self, value):
         self.__decrease_learning_rate = value
 
+    def __learning_schedule(self, t):
+        return 1 / t
+        
     def call(self, loss, parameters, data, labels):
         for item in range(data.shape[0]):
             random_index = np.random.randint(data.shape[0])
@@ -22,10 +33,10 @@ class StochasticGradientDescent(Optimizer):
             y_i = labels[random_index: random_index + 1]      # it is equivalent to the labels[random_index].reshape(1, -1)
 
             gradients = self._partial_derivative(loss, parameters, x_i, y_i)
-            new_parameters = self._learning_rate * gradients
+            new_parameters = self.__learning_rate * gradients
 
             if self.__decrease_learning_rate:
-                self._learning_rate = self._learning_schedule((1 / (self._learning_rate + 1)) * data.shape[0])
+                self.__learning_rate = self.__learning_schedule((1 / (self.__learning_rate + 1)) * data.shape[0])
 
         return new_parameters
 
