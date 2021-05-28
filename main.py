a7c1224328081262165587748235593849260fa6
recep-yildirim <0.recepyildirim@gmail.com>
2021-05-29 00:19:23 +0300

    LinearRegression's inference method has been tried.

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
 # This file is just a temporary file to try optimizers, losses, models etc.
 
-from Optimizers.StochasticGradientDescent import StochasticGradientDescent
 import numpy as np
 
 np.random.seed(42)
 
-X = 2 * np.random.rand(100, 3)
-y = np.array(4 + (3 * X[:, 0]) + (2 * X[:, 1]) + (5 * X[:, 2]) + np.random.rand(100)).reshape(100, 1)
+X_train = 2 * np.random.rand(100, 3)
+y_train = np.array(4 + (3 * X_train[:, 0]) + (2 * X_train[:, 1]) + (5 * X_train[:, 2])).reshape(100, 1)
+
+X_test = 3 * np.random.rand(10, 3)
+y_test = np.array(4 + (3 * X_test[:, 0]) + (2 * X_test[:, 1]) + (5 * X_test[:, 2])).reshape(10, 1)
 
 from Optimizers import BatchGradientDescent
 from Models import LinearRegression
 from Losses import MSE
 
-lin_reg = LinearRegression(100, StochasticGradientDescent(32, 0.01), MSE())
-print(lin_reg(X, y))
+lin_reg = LinearRegression(2000, BatchGradientDescent(0.01), MSE())
+lin_reg(X_train, y_train)
+predictions = lin_reg.inference(X_test)
+
+loss = MSE()
+print(loss(y_test, predictions))
+
+for i,j in zip(y_test, predictions):
+    print(f"actual: {i} - prediction: {j}")
