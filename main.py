75c71f02475916c21e789987ddbaee9c1b078de4
recep-yildirim <0.recepyildirim@gmail.com>
2021-06-01 01:29:04 +0300

    LogisticRegression has been tried.

--- main.py
+++ main.py
@@ -10,16 +10,13 @@ y_train = np.array(4 + (3 * X_train[:, 0]) + (2 * X_train[:, 1]) + (5 * X_train[
 X_test = 3 * np.random.rand(10, 3)
 y_test = np.array(4 + (3 * X_test[:, 0]) + (2 * X_test[:, 1]) + (5 * X_test[:, 2])).reshape(10, 1)
 
-from Optimizers import BatchGradientDescent
-from Models import LinearRegression
+from Optimizers import MiniBatchGradientDescent
+from Models import LogisticRegression
 from Losses import MSE
 
-lin_reg = LinearRegression(2000, BatchGradientDescent(0.01), MSE())
-lin_reg(X_train, y_train)
-predictions = lin_reg.inference(X_test)
+log_reg = LogisticRegression(2000, MiniBatchGradientDescent(0.1), MSE())
+log_reg(X_train, y_train)
+predictions = log_reg.inference(X_test)
 
-loss = MSE()
-print(loss(y_test, predictions))
-
-for i,j in zip(y_test, predictions):
-    print(f"actual: {i} - prediction: {j}")
+print("#-------------------- Parameters ------------------------#")
+print(log_reg.parameters)
