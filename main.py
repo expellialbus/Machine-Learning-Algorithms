453cf6f6445090da81d9532e8b89a086f709325b
Recep YILDIRIM <0.recepyildirim@gmail.com>
2021-06-04 01:04:44 +0300

    New loss functions have been tried.

--- main.py
+++ main.py
@@ -10,13 +10,16 @@ y_train = np.array(4 + (3 * X_train[:, 0]) + (2 * X_train[:, 1]) + (5 * X_train[
 X_test = 3 * np.random.rand(10, 3)
 y_test = np.array(4 + (3 * X_test[:, 0]) + (2 * X_test[:, 1]) + (5 * X_test[:, 2])).reshape(10, 1)
 
-from Optimizers import MiniBatchGradientDescent
-from Models import LogisticRegression
-from Losses import MSE
+from Optimizers import BatchGradientDescent
+from Models.LinearModels import LogisticRegression
+from Losses import CosineSimilarity as CS
 
-log_reg = LogisticRegression(2000, MiniBatchGradientDescent(0.1), MSE())
+log_reg = LogisticRegression(2000, BatchGradientDescent(0.1), CS())
 log_reg(X_train, y_train)
 predictions = log_reg.inference(X_test)
 
 print("#-------------------- Parameters ------------------------#")
 print(log_reg.parameters)
+
+print("#---------------------- Losses --------------------------#")
+print(CS()(y_test, predictions))
