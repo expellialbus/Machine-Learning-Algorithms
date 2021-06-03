b46b11f9ded66f43eb5a445024f93117dec498e4
Recep YILDIRIM <0.recepyildirim@gmail.com>
2021-06-04 01:01:04 +0300

    New loss functions have been imported.

--- Losses/__init__.py
+++ Losses/__init__.py
@@ -11,5 +11,8 @@ def get_parent_dir(n=0):
 os.sys.path.append(get_parent_dir())
 
 from Loss import Loss
-from MSE import MSE
-from MAE import MAE
+from MeanSquaredError import MeanSquaredError
+from MeanAbsoluteError import MeanAbsoluteError
+from MeanAbsolutePercentageError import MeanAbsolutePercentageError
+from MeanSquaredLogarithmicError import MeanSquaredLogarithmicError
+from CosineSimilarity import CosineSimilarity
