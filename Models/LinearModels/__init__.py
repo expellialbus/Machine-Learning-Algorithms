b6522b4a074a17ff4ee3cddce1c0607ddcbc6b75
Recep YILDIRIM <0.recepyildirim@gmail.com>
2021-06-03 23:50:57 +0300

    LinearModels package has been created.

---
+++ Models/LinearModels/__init__.py
@@ -0,0 +1,14 @@
+import os
+
+def get_parent_dir(n=0):
+    current_path = os.path.dirname(os.path.abspath(__file__))
+
+    for _ in range(n):
+        current_path = os.path.dirname(current_path)
+
+    return current_path
+
+os.sys.path.append(get_parent_dir(1))
+
+from .LinearRegression import LinearRegression
+from .LogisticRegression import LogisticRegression
