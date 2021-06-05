05a46c1b5377d290a4750cc645276bf6f4972bfd
Recep YILDIRIM <0.recepyildirim@gmail.com>
2021-06-05 23:32:47 +0300

    Momentum optimizer has been tried.

--- main.py
+++ main.py
@@ -10,11 +10,11 @@ y_train = np.array(4 + (3 * X_train[:, 0]) + (2 * X_train[:, 1]) + (5 * X_train[
 X_test = 3 * np.random.rand(10, 3)
 y_test = np.array(4 + (3 * X_test[:, 0]) + (2 * X_test[:, 1]) + (5 * X_test[:, 2])).reshape(10, 1)
 
-from Optimizers import BatchGradientDescent
-from Models.LinearModels import LogisticRegression
-from Losses import CosineSimilarity as CS
+from Optimizers import Momentum
+from Models.LinearModels import LinearRegression
+from Losses import MeanSquaredError as MSE
 
-log_reg = LogisticRegression(2000, BatchGradientDescent(0.1), CS())
+log_reg = LinearRegression(2000, Momentum(0.1), MSE())
 log_reg(X_train, y_train)
 predictions = log_reg.inference(X_test)
 
@@ -22,4 +22,4 @@ print("#-------------------- Parameters ------------------------#")
 print(log_reg.parameters)
 
 print("#---------------------- Losses --------------------------#")
-print(CS()(y_test, predictions))
+print(MSE()(y_test, predictions))
