50572a7b2e66eaf97b9422c40fbecd53005dbf5c
Recep YILDIRIM <0.recepyildirim@gmail.com>
2021-06-07 01:31:07 +0300

    AdaGrad has been tried.

--- main.py
+++ main.py
@@ -10,11 +10,11 @@ y_train = np.array(4 + (3 * X_train[:, 0]) + (2 * X_train[:, 1]) + (5 * X_train[
 X_test = 3 * np.random.rand(10, 3)
 y_test = np.array(4 + (3 * X_test[:, 0]) + (2 * X_test[:, 1]) + (5 * X_test[:, 2])).reshape(10, 1)
 
-from Optimizers import Momentum
+from Optimizers import AdaGrad
 from Models.LinearModels import LinearRegression
 from Losses import MeanSquaredError as MSE
 
-log_reg = LinearRegression(2000, Momentum(0.1), MSE())
+log_reg = LinearRegression(2000, AdaGrad(5), MSE())
 log_reg(X_train, y_train)
 predictions = log_reg.inference(X_test)
 
