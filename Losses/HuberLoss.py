8d0bdfd360dd734b92649efb64e2b877e5f9cc2b
Recep YILDIRIM <0.recepyildirim@gmail.com>
2021-06-15 21:34:20 +0300

    Shape bug has been fixed.

--- Losses/HuberLoss.py
+++ Losses/HuberLoss.py
@@ -16,14 +16,15 @@ class HuberLoss(Loss):
 
     def call(self, true_labels, predicted_labels):
         loss = list()
-        for true_label, predicted_label in zip(true_labels, predicted_labels):
+        for true_label, predicted_label in zip(true_labels.ravel(), predicted_labels.ravel()):
             if np.abs(predicted_label - true_label) <= self.__delta:
                 loss.append((1 / 2) * np.power((predicted_label - true_label), 2))
             else:
                 loss.append((self.__delta * np.abs(predicted_label - true_label)) - ((1 / 2) * np.power(self.__delta, 2)))
 
-       # print(np.array(loss).shape)
-        return np.array(loss)
+        loss = np.array(loss)[:, np.newaxis]    # converts the loss list to a numpy array as a column vector
+
+        return np.mean(loss)
 
     def __call__(self, true_labels, predicted_labels):
         return self.call(true_labels, predicted_labels)
