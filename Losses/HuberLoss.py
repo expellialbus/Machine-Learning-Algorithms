72ac5c6c46787598d0c5259f37f33a4ee4aca878
Recep YILDIRIM <0.recepyildirim@gmail.com>
2021-06-15 21:52:07 +0300

    List usage has been altered with the scalar value.

--- Losses/HuberLoss.py
+++ Losses/HuberLoss.py
@@ -15,16 +15,14 @@ class HuberLoss(Loss):
         self.__delta = value
 
     def call(self, true_labels, predicted_labels):
-        loss = list()
+        loss = 0
         for true_label, predicted_label in zip(true_labels.ravel(), predicted_labels.ravel()):
             if np.abs(predicted_label - true_label) <= self.__delta:
-                loss.append((1 / 2) * np.power((predicted_label - true_label), 2))
+                loss += (1 / 2) * np.power((predicted_label - true_label), 2)
             else:
-                loss.append((self.__delta * np.abs(predicted_label - true_label)) - ((1 / 2) * np.power(self.__delta, 2)))
+                loss += (self.__delta * np.abs(predicted_label - true_label)) - ((1 / 2) * np.power(self.__delta, 2))
 
-        loss = np.array(loss)[:, np.newaxis]    # converts the loss list to a numpy array as a column vector
-
-        return np.mean(loss)
+        return loss / true_labels.shape[0]
 
     def __call__(self, true_labels, predicted_labels):
         return self.call(true_labels, predicted_labels)
