45224dd65e86b9e01d00a4a6a216d63139841d3a
Recep YILDIRIM <0.recepyildirim@gmail.com>
2021-06-20 22:26:47 +0300

    Formula has been fixed.

--- Losses/MeanAbsolutePercentageError.py
+++ Losses/MeanAbsolutePercentageError.py
@@ -4,7 +4,7 @@ from Losses import Loss
 
 class MeanAbsolutePercentageError(Loss):
     def call(self, true_labels, predicted_labels):
-        return np.mean(np.abs((predicted_labels - true_labels) / true_labels))
+        return 100 * np.mean(np.abs((predicted_labels - true_labels) / true_labels))
 
     def __call__(self, true_labels, predicted_labels):
         return self.call(true_labels, predicted_labels)
