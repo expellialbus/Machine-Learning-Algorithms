e04542eededd4d17149a36a05f412b42fa09536a
Recep YILDIRIM <0.recepyildirim@gmail.com>
2021-06-20 23:40:16 +0300

    MeanAbsoluteRelativeError has been tried.

--- main.py
+++ main.py
@@ -23,9 +23,9 @@ plt.show()
 
 from Optimizers import AdaMax
 from Models.LinearModels import LinearRegression
-from Losses import Quantile
+from Losses import MARE
 
-lin_reg = LinearRegression(2000, AdaMax(), Quantile())
+lin_reg = LinearRegression(2000, AdaMax(), MARE())
 lin_reg(X_train, y_train)
 predictions = lin_reg.inference(X_test)
 
@@ -33,4 +33,4 @@ print("#-------------------- Parameters ------------------------#")
 print(lin_reg.parameters)
 
 print("#---------------------- Losses --------------------------#")
-print(Quantile()(y_test, predictions))
+print(MARE()(y_test, predictions))
