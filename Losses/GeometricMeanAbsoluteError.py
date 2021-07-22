e15c812fe77e5e05dc2c2ebcbdf32dffae549dcc
Recep YILDIRIM <0.recepyildirim@gmail.com>
2021-07-22 19:50:48 +0300

    Equation parenthesis has been added.

--- Losses/GeometricMeanAbsoluteError.py
+++ Losses/GeometricMeanAbsoluteError.py
@@ -4,7 +4,7 @@ from Losses import Loss
 
 class GeometricMeanAbsoluteError(Loss):
     def call(self, true_labels, predicted_labels):
-        return np.power(np.prod(np.abs(true_labels - predicted_labels)), 1 / true_labels.shape[0])
+        return np.power(np.prod(np.abs(true_labels - predicted_labels)), (1 / true_labels.shape[0]))
 
     def __call__(self, true_labels, predicted_labels):
         return self.call(true_labels, predicted_labels)
