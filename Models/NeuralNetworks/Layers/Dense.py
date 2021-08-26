dc14310e46ca5d503d4dc1217b9564b8e312521e
Recep YILDIRIM <0.recepyildirim@gmail.com>
2021-08-26 23:02:42 +0300

    Forward method has been completed.

--- Models/NeuralNetworks/Layers/Dense.py
+++ Models/NeuralNetworks/Layers/Dense.py
@@ -1,28 +1,32 @@
 import numpy as np
 
 from ..Layers import Layer
+from ..Activations import Activation
 
 class Dense(Layer):
     def __init__(self, n_neurons, activation=None):
         super().__init__(n_neurons, activation)
 
-        self.weights = np.random.randn(self.inputs.shape[0] + 1, n_neurons)     # the plus one for the bias node
+        self._weights = np.random.randn(self._inputs.shape[1] + 1, n_neurons)     # the plus one for the bias node
 
     @property
     def weights(self):
-        return self.weights
+        return self._weights
 
     @weights.setter
     def weights(self, value):
-        self.weights = value
+        self._weights = value
 
     def forward(self):
-        X = np.c_[self.inputs, np.ones((self.inputs.shape[0], 1))]
+        X = np.c_[self._inputs, np.ones((self._inputs.shape[0], 1))]
 
-        result = X.dot(self.weights)
+        result = X.dot(self._weights)
 
         if self.activation:
-            pass
+            activation = Activation.get_activation(self._activation)
 
+            result = activation(result)
+
+        self._outputs = result
 
 
