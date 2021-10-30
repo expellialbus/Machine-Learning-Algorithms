9879185bb0cd40fd151c1642670e1ff051372ad2
Recep YILDIRIM <0.recepyildirim@gmail.com>
2021-10-30 23:42:09 +0300

    Feed forward operation has been completed.

--- Models/NeuralNetworks/Sequential.py
+++ Models/NeuralNetworks/Sequential.py
@@ -4,21 +4,38 @@ class Sequential(Layer):
     def __init__(self):
         self.layers = list()
 
+        self.__loss = None
+        self.__optimizer = None
+
+
     def add(self, layer):
         self.layers.append(layer)
 
     def build(self, loss, optimizer):
-        pass
+        self.__loss = loss
+        self.__optimizer = optimizer
 
     def call(self, data, labels):
         self.initialize_layers(data.shape[1])
+        self.inputs = data
 
-        for layer in self.layers:
-            layer.inputs = data
-            layer.forward()
-            data = layer.outputs
+        self.forward()
+        loss = self.__loss(labels, self.outputs)
+
 
     def initialize_layers(self, shape):
         for layer in self.layers:
             layer.initialize_weights(shape)
             shape = layer.n_neurons
+
+    def forward(self):
+        data = self.inputs
+        for layer in self.layers:
+            layer.inputs = data
+            layer.forward()
+            data = layer.outputs
+
+        self.outputs = data
+
+    def backward(self, optimizer, delta):
+        pass
