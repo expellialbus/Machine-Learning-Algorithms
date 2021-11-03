850e67a56b8a0f0bc6636f4967ec4d4db5cd069e
Recep YILDIRIM <0.recepyildirim@gmail.com>
2021-11-03 22:21:29 +0300

    New changes have been tried.

--- main.py
+++ main.py
@@ -10,25 +10,39 @@ y_train = np.array(4 + (3 * X_train[:, 0]) + (2 * X_train[:, 1]) + (5 * X_train[
 X_test = 3 * np.random.rand(10, 3)
 y_test = np.array(4 + (3 * X_test[:, 0]) + (2 * X_test[:, 1]) + (5 * X_test[:, 2])).reshape(10, 1)
 
-"""
-#visualizing of data
-import matplotlib as mpl
-mpl.use("Qt5Agg")
-import matplotlib.pyplot as plt
 
-for color, dim in zip(("blue", "green", "red"), range(X_train.shape[1])):
-    plt.scatter(X_train[:, dim], y_train, marker="^", color=color)
-plt.show()
-"""
+#visualizing of data
+# import matplotlib as mpl
+# mpl.use("Qt5Agg")
+# import matplotlib.pyplot as plt
+#
+# for color, dim in zip(("blue", "green", "red"), range(X_train.shape[1])):
+#     plt.scatter(X_train[:, dim], y_train, marker="^", color=color)
+# plt.show()
+
 
 from Models.NeuralNetworks import Layers
 from Models.NeuralNetworks import Sequential
 
-
 model = Sequential()
-model.add(Layers.Dense(5, "relu"))
-model.add(Layers.Dense(10, "relu"))
+model.add(Layers.Dense(5, "Relu"))
+model.add(Layers.Dense(10, "Relu"))
+model.build("MeanSquaredError", "Adam")
 model.call(X_train, y_train)
 print(model.layers[1].inputs.shape)
 print(model.layers[0].inputs.shape)
 print(model.layers[0].outputs.shape)
+
+
+# from Models.LinearModels import LinearRegression
+# from Optimizers import Adam
+# from Losses import MAE
+#
+# model = LinearRegression(2000, Adam(), MAE())
+# model(X_train, y_train)
+# predictions = model.inference(X_test)
+# print(MAE()(y_test, predictions))
+#
+# predictions = model.inference(X_test)
+# for index in range(len(y_test)):
+#     print(y_test[index], predictions[index])
