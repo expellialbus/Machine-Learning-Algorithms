90fb9fafd3db403f85ddd2ac6b8d8f763955b6e1
Recep YILDIRIM <0.recepyildirim@gmail.com>
2021-11-03 22:21:09 +0300

    Get optimizer method has been created.

--- Optimizers/__init__.py
+++ Optimizers/__init__.py
@@ -1,3 +1,4 @@
+from importlib import import_module
 import os
 
 def get_parent_dir(n=0):
@@ -7,6 +8,12 @@ def get_parent_dir(n=0):
         current_path = os.path.dirname(current_path)
 
     return current_path
+
+def get_optimizer(name):
+    module = import_module(name)
+    optimizer = getattr(module, name)
+
+    return optimizer()  # creates an instance of the class
 
 os.sys.path.append(get_parent_dir())
 
