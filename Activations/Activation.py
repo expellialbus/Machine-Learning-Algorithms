37642642282b49f31d47644c9532cae0c3765d1b
Recep YILDIRIM <0.recepyildirim@gmail.com>
2021-11-03 22:18:43 +0300

    Get activation method has been moved to the __init__.py file.

--- Activations/Activation.py
+++ Activations/Activation.py
@@ -1,14 +1,6 @@
 from abc import ABC, abstractmethod
-from importlib import import_module
 
 class Activation(ABC):
-    @staticmethod
-    def get_activation(name):
-        module = import_module("Activations." + name.capitalize())
-        activation = getattr(module, name.capitalize())
-
-        return activation()     # creates an instance of the class
-
     @abstractmethod
     def call(self, inputs):
         pass
--- Activations/__init__.py
+++ Activations/__init__.py
@@ -1,3 +1,4 @@
+from importlib import import_module
 import os
 
 def get_parent_dir(n=0):
@@ -7,6 +8,12 @@ def get_parent_dir(n=0):
         current_path = os.path.dirname(current_path)
 
     return current_path
+
+def get_activation(name):
+    module = import_module(name)
+    activation = getattr(module, name)
+
+    return activation()     # creates an instance of the class
 
 os.sys.path.append(get_parent_dir())
 
