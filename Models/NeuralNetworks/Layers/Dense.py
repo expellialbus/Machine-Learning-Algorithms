Bugs in backpropagation stage have been fixed.

Models/NeuralNetworks/Layers/Dense.py
@@ -7,6 +7,7 @@ class Dense(Layer):
     def __init__(self, n_neurons, activation=None):
         super().__init__(n_neurons, activation)
         self._weights = None
+        self._prev_weights = None
 
     def initialize_weights(self, shape):
         if self._weights == None:
@@ -20,12 +21,18 @@ class Dense(Layer):
     def weights(self, value):
         self._weights = value
 
+    @property
+    def prev_weights(self):
+        return self._prev_weights
+
     def forward(self):
-        X = np.c_[self._inputs, np.ones((self._inputs.shape[0], 1))]
+        self.inputs = np.c_[self._inputs, np.ones((self._inputs.shape[0], 1))]
 
-        result = X.dot(self._weights)
+        result = self.inputs.dot(self._weights)
 
-        if self.activation:
+        # activation variable is str in the initial state
+        # in the prediction phase it should be controller as not an object
+        if type(self.activation) is str:
             self._activation = get_activation(self._activation)
 
             result = self._activation(result)
@@ -33,4 +40,7 @@ class Dense(Layer):
         self._outputs = result
 
     def backward(self, delta):
-        self._weights -= self._optimizer(self._outputs.T.dot(delta))
+        # previous weights is used in backpropagation and weights for bias term should not be propagated
+        # copying the original weights without the bias term's weights
+        self._prev_weights = np.copy(np.array(self.weights[:-1, :]))
+        self._weights -= self._optimizer(self._inputs.T.dot(delta), parameters=self.weights)